12c2172d746a22c43d67e4ce5f9de6c6a206070a
EdgescaleGitBot <git@edgescale.dev>
2026-05-05 15:29:38 +0400

    removes departments migrations

--- migrations/004_create_departments.down.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS idx_tenant_member_department;
-DROP TABLE IF EXISTS member_departments;
-
-DROP INDEX IF EXISTS idx_department_tenant_slug;
-DROP INDEX IF EXISTS idx_department_tenant_id;
-DROP TABLE IF EXISTS departments;
